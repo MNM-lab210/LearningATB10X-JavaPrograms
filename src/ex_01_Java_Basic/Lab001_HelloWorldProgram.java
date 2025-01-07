1442a6e7db11ec3110ffec1f94f20adb5cdfac5f
MNM-lab210 <padmajamyneni@gmail.com>
2025-01-07 18:42:21 +0530

    git remote add origin https://github.com/MNM-lab210/LearningATB10X-JavaPrograms.git git branch -M main git push -u origin main

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -4,7 +4,7 @@ public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hi,Myneni");
 
-        System.out.println("SaiRam");
+
 
         }
     }

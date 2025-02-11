1f8deeb330d8994dc90ab8468d78a25c7b983078
MNM-lab210 <padmajamyneni@gmail.com>
2025-02-11 09:37:02 +0530

    TO

--- src/ex_02_TO/Lab_009_IncreandDecre.java
+++ src/ex_02_TO/Lab_009_IncreandDecre.java
@@ -2,9 +2,15 @@ package ex_02_TO;
 
 public class Lab_009_IncreandDecre {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 14;
         System.out.println(++a + a++ +a++);
         System.out.println(a);
+
+
+        int a1=20;
+        System.out.println(--a1+ a1++ +a1--);
+        System.out.println(a1);
+
 
     }
 }
